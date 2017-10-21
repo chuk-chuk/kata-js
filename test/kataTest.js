704e09b86dcb6c05496919b4fee1e39d5366d456
chuk-chuk <yulia.v.litvin@gmail.com>
2017-10-21 14:55:02 +0100

    input always should be an integer

--- test/kataTest.js
+++ test/kataTest.js
@@ -10,4 +10,10 @@ describe("Kata", () => {
   it('returns one if length is less than two', () => {
     expect(kata.summation(1)).to.equal(1)
   })
+
+  it('always should be an integer', () => {
+    expect(kata.summation(1)).to.be.a( 'number' );
+  })
+
+
 })
